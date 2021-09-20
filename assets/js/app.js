21103179bd79f1a770a65b623a20b92f20e60b2d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-20 09:26:10 +0530

    script

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,12 +10,44 @@ actionBtn.addEventListener('click', (e) => {
       <div class='dialogue'>
         <h1>The <span class='accent-color'>big bang</span> is about to happen ! 💥</h1>
         <p>Enter your email, To get <span class='accent-color'>notified</span> on the launch of the app</p>
-        <form id='gform' method='POST' action='https://script.google.com/macros/s/AKfycbzU1-QhpEauQLcPdI6fw0GmzNgopWbGMfUieYEuxG1945Eewpqq6FRMXOvg-0hTN1nt/exec'>
+        <form id='dialogue-form'>
           <input name='email' id='dialogue-input' placeholder='johndoe@gmail.com' type='email' required />
-          <button type='submit'>Notify Me !</button>
+          <button id='dialogue-btn' type='submit'>Notify Me !</button>
         </form>
       </div>
     `,
     showConfirmButton: false,
+  })
+
+  // Listen & submit 
+  document.querySelector('#dialogue-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    document.getElementById('dialogue-btn').innerHTML = 'Loading ...'
+
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbzU1-QhpEauQLcPdI6fw0GmzNgopWbGMfUieYEuxG1945Eewpqq6FRMXOvg-0hTN1nt/exec',
+      data: $("#dialogue-form").serialize(),
+      method: "post",
+      success: function (response) {
+        Swal.fire({
+          icon: 'success',
+          background: '#2d3748',
+          html: `<h2 class='dialogue-info'>
+          You will be notified when app get launched 🥳
+          </h2>
+          `,
+          showConfirmButton: false,
+        })
+      },
+      error: function (err) {
+        Swal.fire({
+          icon: 'error',
+          background: '#2d3748',
+          html: `<h2 class='dialogue-info'>Oops, failed to submit email</h2>`,
+          showConfirmButton: false,
+        })
+      }
+    })
   })
 });
