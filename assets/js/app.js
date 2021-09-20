3610e4d36d81f8bcf71c5ba1cb783f529e945e1e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-20 08:48:49 +0530

    form setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,10 +10,12 @@ actionBtn.addEventListener('click', (e) => {
       <div class='dialogue'>
         <h1>The <span class='accent-color'>big bang</span> is about to happen ! 💥</h1>
         <p>Enter your email, To get <span class='accent-color'>notified</span> on the launch of the app</p>
-        <input placeholder='johndoe@gmail.com' />
-        <button>Notify Me !</button>
+        <form id='gform' method='POST' action='https://script.google.com/macros/s/AKfycbzU1-QhpEauQLcPdI6fw0GmzNgopWbGMfUieYEuxG1945Eewpqq6FRMXOvg-0hTN1nt/exec'>
+          <input name='email' id='dialogue-input' placeholder='johndoe@gmail.com' type='email' required />
+          <button type='submit'>Notify Me !</button>
+        </form>
       </div>
     `,
     showConfirmButton: false,
   })
-})
+});
